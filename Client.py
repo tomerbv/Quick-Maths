6045410e7469101e4dc5896b65bb219f96305c83
tomer <tomerbv@gmail.com>
2021-12-27 17:25:16 +0200

    Initial test run 1.0

--- Client.py
+++ Client.py
@@ -1,3 +1,4 @@
+import msvcrt
 import socket
 
 
@@ -12,7 +13,7 @@ class Client:
         self.name = "TomOmer"
 
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_socket.bind(('', self.looking_port))
 
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,32 +25,36 @@ class Client:
         while not self.server_found:
 
             print("Client started, listening for offer requests...")
-            verifaction_tcp,adress = self.udp_socket.recv('UTF-8')
+            verifaction_tcp, adress = self.udp_socket.recvfrom(1024)
             recieved_cookie = hex(int(verifaction_tcp.hex()[:8],16))
-            recieved_type=verifaction_tcp.hex()[10:]
+            recieved_type = verifaction_tcp.hex()[9:10]
             recieved_port = verifaction_tcp.hex()[10:]
-            if(recieved_cookie==hex(self.magic_cookie) and int(recieved_type)==self.offer_message_type):
-                self.tcp_port=int(recieved_port,16)
-                self.ip=adress[0]
-            print("Recieved offer from " + str(self.ip) + ", attempting to connect...\n")
-            self.server_found=True
+
+            if(recieved_cookie == hex(self.magic_cookie) and int(recieved_type)==self.offer_message_type):
+                self.tcp_port = int(recieved_port,16)
+                self.ip = adress[0]
+                print("Recieved offer from " + str(self.ip) + ", attempting to connect...\n")
+                self.server_found = True
 
 
 
     def connecting_to_server(self):
-        print("Recieved offer from " + str(self.ip) + ", attempting to connect...to my tcp port" + str(self.tcp_port + '\n'))
+        # print("Recieved offer from " + str(self.ip) + ", attempting to connect...to my tcp port" + str(self.tcp_port + '\n'))
 
         self.tcp_socket.connect((self.ip,self.tcp_port))
-        team_msg=bytes(self.name,'UTF-8')
+        team_msg = bytes(self.name,'UTF-8')
         self.tcp_socket.send((team_msg))
 
     def game_mode(self):
-        return
+        msg = self.tcp_socket.recv(1024)
+        print(msg.decode('UTF-8'))
+        self.tcp_socket.send(msvcrt.getch())
 
 
     def start(self):
         self.looking_for_server()
         self.connecting_to_server()
+        self.game_mode()
 
 if __name__ == "__main__":
     while True:
--- Server.py
+++ Server.py
@@ -6,15 +6,14 @@ from threading import Thread
 class Server:
     def __init__(self, tcp_port):
         self.looking_port = 13117
-        # self.tcp_port = tcp_port
+        self.tcp_port = tcp_port
 
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.ip = socket.gethostbyname(socket.gethostname())
-        self.msg = bytes(0xabcddcba) + bytes(0x2) + tcp_port
-
+        self.msg = 0xabcddcba.to_bytes(byteorder='big', length=4) + 0x2.to_bytes(byteorder='big', length=1) + tcp_port.to_bytes(byteorder='big', length=2)
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.bind("", tcp_port)
+        self.tcp_socket.bind(("" , tcp_port))
 
         self.client1 = None
         self.client1_name = None
@@ -23,11 +22,11 @@ class Server:
 
     def broadcast(self):
         while not self.players_ready():
-            self.udp_socket.sendto(self.msg, self.looking_port)
+            self.udp_socket.sendto(self.msg, ('255.255.255.255', self.looking_port))
             time.sleep(1)
 
     def players_ready(self):
-        return self.client1_addr is not None and self.client2 is not None
+        return self.client1 is not None and self.client2 is not None
 
     def waiting_for_clients(self):
         print("Server started, listening on IP address " + self.ip)
@@ -39,10 +38,12 @@ class Server:
             if self.client1 is None:
                 self.client1, address = self.tcp_socket.accept()
                 self.client1_name = self.client1.recv(1024).decode('UTF-8')
+                print("connected 1")
 
             elif self.client2 is None:
                 self.client2, address = self.tcp_socket.accept()
                 self.client2_name = self.client2.recv(1024).decode('UTF-8')
+                print("connected 2")
 
         broad.join()
 
@@ -51,13 +52,19 @@ class Server:
 
 
     def game_mode(self):
-        print(f"Received offer from {self.client1_name} and {self.client2_name}, attempting to connect...")
+        self.client1.send(bytes("hello client", 'UTF-8'))
+        self.client2.send(bytes("hello client", 'UTF-8'))
+        ans1 = self.client1.recv(1024)
+        ans2 = self.client2.recv(1024)
+        print(ans1.decode('UTF-8'))
+        print(ans2.decode('UTF-8'))
 
     def start(self):
         self.waiting_for_clients()
+        print(f"Received offer from {self.client1_name} and {self.client2_name}, attempting to connect...")
         summary = self.game_mode()
         print("done")
 
 while True:
     server = Server(18121)
-    server.start_server()
+    server.start()
