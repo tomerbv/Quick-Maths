0e2408068ab7a2fca09c028bdc9b3dd636bd3cd7
tomer <tomerbv@gmail.com>
2021-12-27 14:55:01 +0200

    Initial 1.1

--- Client.py
+++ Client.py
@@ -1,19 +1,27 @@
-
+import socket
 
 
 class Client:
     def __init__(self):
+        self.looking_port = 13117
+
         self.server_found = False;
 
+        self.name = "TomOmer"
 
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.udp_socket.bind(('', self.looking_port))
 
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.magic_cookie = 0xabcddcba
         self.offer_message_type = 0x2
 
     def looking_for_server(self):
         while not self.server_found:
-            print("Client started, listening for offer requests...")
+            msg = self.udp_socket.recv("utf-8")
+
 
     def connecting_to_server(self):
         print("Received offer from , attempting to connect...")
--- Server.py
+++ Server.py
@@ -1,17 +1,19 @@
-
+import socket
+import socketserver
 
 
 
 
 class Server:
     def __init__(self):
+        socket
 
 
 
 
 
 
-
+        # this.ip =
         self.magic_cookie = 0xabcddcba
         self.offer_message_type = 0x2
 
@@ -25,9 +27,10 @@ class Server:
 
 
 
+
     def waiting_for_clients(self):
         while not self.server_found:
-            print("Client started, listening for offer requests...")
+            print("Server started, listening on IP address "  )
 
     def hame_mode(self):
         print("Received offer from 172.1.0.4, attempting to connect...")
